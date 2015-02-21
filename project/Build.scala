2e68ee920ae48a64f0da1995089205f2cf45ed30
wsoczynski <wmsoczynski@gmai.com>
2015-02-21 12:54:42 +0000

    update to latest project settings

--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,17 @@
-import sbt._
-import Keys._
-import PlayProject._
-
-object ApplicationBuild extends Build {
-
-  val appName = "yabe"
-  val appVersion = "1.0-SNAPSHOT"
-
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    "com.octo.captcha" % "jcaptcha" % "1.0")
-
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here      
-  )
-
-}
+// import sbt._
+// import Keys._
+// import PlayProject._
+//
+// object ApplicationBuild extends Build {
+//
+//   val appName = "yabe"
+//   val appVersion = "1.0-SNAPSHOT"
+//
+//   val appDependencies = Seq(
+//     // Add your project dependencies here,
+//     "com.octo.captcha" % "jcaptcha" % "1.0")
+//
+//   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings( // Add your own project settings here
+//   )
+//
+// }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,6 +10,7 @@ resolvers += "Maven central repository" at "http://repo1.maven.org/maven2/"
 resolvers += "jahia org repository" at "http://maven.jahia.org/maven2/"
 
 // Use the Play sbt plugin for Play projects
-addSbtPlugin("play" % "sbt-plugin" % "2.0-RC3")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.6")
 
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.5.0")
 // libraryDependencies += "com.octo.captcha" % "jcaptcha" % "1.0"
